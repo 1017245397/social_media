e11fa0889aa94b252fc347b245594f14d5daee60
Elisa <elisa0503@gmail.com>
2022-11-26 13:56:29 -0500

    feat: Creación de modelos y serializadores para empresas

--- company/models.py
+++ company/models.py
@@ -1,19 +1,74 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
 class CompanyModel(models.Model):
     '''
     Modelo de empresas
     '''
-    nit=models.CharField(max_length=20,unique=True,verbose_name="nit")
-    description= models.TextField(verbose_name="descripcion", blank=True)
-    mision=models.TextField("mision",blank=True)
-    vision=models.TextField("vision",blank=True)
-    name= models.CharField("nombre",max_length=50)
+    nit=models.CharField("NIT", max_length=20, unique=True)
+    description= models.TextField("Descripción", blank=True)
+    mision=models.TextField("Misión", blank=True)
+    vision=models.TextField("Visión", blank=True)
+    name=models.CharField("Nombre", max_length=50)
+    facebook=models.CharField("Facebook", max_length=150, blank=True)
+    linkedin=models.CharField("linkedin", max_length=150, blank=True)
+    instagram=models.CharField("Instagram", max_length=150, blank=True)
+    twitter=models.CharField("Twitter", max_length=150, blank=True)
+    web_site=models.CharField("Sitio Web", max_length=150, blank=True)
 
     class Meta:
-        verbose_name="empresa"
-        verbose_name_plural="empresas"
+        verbose_name="Empresa"
+        verbose_name_plural="Empresas"
         
 
+class ContactCompanyModel(models.Model):
+    '''
+    Modelo de información de contacto para las empresas
+    '''
+    address=models.CharField("Direccion", max_length=50, blank=True)
+    cell_phone=models.CharField("Celular", max_length=10, blank=True)
 
+
+    class Meta:
+        verbose_name="Información de contacto para empresa"
+        verbose_name_plural="Informacion de contacto para empresas"
+
+
+class CategoryCompanyModel(models.Model):
+    '''
+    Modelo de categorias de las compañias
+    '''
+    name=models.CharField("Nombre", max_length=150 )
+    description= models.TextField("Descripción", max_length=450)
+    companies=models.ManyToManyField(CompanyModel)
+
+    class Meta:
+        verbose_name="Categoria de la empresa"
+        verbose_name_plural="Categorias de las empresas"
+
+    
+class GaleryCompanyModel(models.Model):
+    '''
+    Modelo galeria de fotos de la compañia
+    '''
+    photo=models.FileField(upload_to='/Company')
+    company=models.ForeignKey(CompanyModel)
+
+    class Meta:
+        verbose_name="Galeria de la compañia"
+        verbose_name_plural="Galerias de las compañias"
+
+
+class CommentCompanyModel(models.Model):
+    '''
+    Modelo de comentarios
+    '''
+    comment=models.TextField("Comentarios", max_length=500)
+    user=models.ForeignKey(User)
+
+    class Meta:
+        verbose_name="Comentario de usuario a empresa"
+        verbose_name="Comentarios de usuarios a empresas"
+
+    
--- company/serializers.py
+++ company/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from company.models import CompanyModel
-
-
+from company.models import CompanyModel, ContactCompanyModel, CategoryCompanyModel , \
+    GaleryCompanyModel , CommentCompanyModel
 
 class CompanySerializer(serializers.ModelSerializer):
     '''serializador de companias '''
@@ -9,4 +8,39 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyModel
         fields = '__all__'
+         
+
+class ContactCompanySerializer(serializers.ModelSerializer):
+    '''serializador de contacto de companias '''
+
+    class Meta:
+        model = ContactCompanyModel
+        fields = '__all__'
+
+
+class CategoryCompanySerializer(serializers.ModelSerializer):
+    '''serializador de categorias de companias '''
+
+    class Meta:
+        model = CategoryCompanyModel
+        fields = '__all__'
+
+
+class GaleryCompanySerializer(serializers.ModelSerializer):
+    '''serializador de galeria de companias '''
+
+    class Meta:
+        model = GaleryCompanyModel
+        fields = '__all__'
+
+
+class CommentCompanySerializer(serializers.ModelSerializer):
+    '''serializador de comentarios del usuario'''
+
+    class Meta:
+        model = CommentCompanyModel
+        fields = '__all__'
+
+
+
 
