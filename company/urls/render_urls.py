97a7a2ad17428900fc709444d7c9670a0cffaaa0
ricardo <rarango@mercadolibre.com.co>
2022-12-04 14:00:34 -0500

    feat: template y boostrap added

--- company/urls/render_urls.py
+++ company/urls/render_urls.py
@@ -1,7 +1,8 @@
-from company.views.render_views import CompanyTemplateView
+from company.views.render_views import CompanyTemplateView, ExampleTemplateView
 from django.urls import path
 
 urlpatterns = [
-    path('', CompanyTemplateView.as_view(),name='company')
-    ]
+    path('', CompanyTemplateView.as_view(), name='company'),
+    path('example', ExampleTemplateView.as_view(), name='example')
+]
 
--- company/views/render_views.py
+++ company/views/render_views.py
@@ -4,7 +4,5 @@ from django.views.generic.base import TemplateView
 class CompanyTemplateView(TemplateView):
     template_name = "company.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        return context
+class ExampleTemplateView(TemplateView):
+    template_name = "example.html"
