81d49ba17547cff57b19e67b0a62f88249403e6e
Elisa <elisa0503@gmail.com>
2022-12-30 19:03:06 -0500

    login

--- company/forms.py
+++ company/forms.py
@@ -1,5 +1,3 @@
-from core.form import BaseForm
-from company.models import CompanyModel, GaleryCompanyModel ,ContactCompanyModel, CategoryCompanyModel
 from django import forms
 
 
@@ -21,56 +19,4 @@ class RegisterCompanyForm(forms.Form):
     instagram=forms.CharField(label="Instagram", max_length=150)
     twitter=forms.CharField(label="Twitter", max_length=150)
     web_site=forms.CharField(label="Sitio Web", max_length=150)
-
-"""
-    logo= forms.FileField(upload_to='logos')
-    photo=forms.FileField(upload_to='company')
-    document=forms.CharField(label="Numero de Identificación", max_length=20, unique=True)
-    first_names= forms.CharField(label="Nombres", max_length= 150)
-    last_names=forms.CharField(label="Apellidos", max_length=150)
-    birthdate=forms.DateField(label="Fecha de Nacimiento")
-    phone_number=forms.CharField(label="Numero de telefono", max_length=15)
-    email=forms.CharField(label="Correo", max_length=50)
-"""
-"""
-    civil_status= forms.CharField("Estado Civil", max_length=20, choices = CIVIL_ESTATUS_CHOICES)
-    gender=forms.CharField("Genero", max_length=10, choices = GENDER_CHOICES)   
-    type_document=forms.CharField("Tipo de Documento", max_length=10, choices = TYPE_DOCUMENT_CHOICES)# por que estos son distintos al foreingKEY
-    user=forms.OneToOneField(User, on_delete=models.CASCADE)
-    user=forms.ForeignKey(User, on_delete=models.CASCADE)
-    company=forms.ForeignKey(CompanyModel, on_delete= models.CASCADE)
-"""
-
-
-
-"""# Create the form class.
-class CompanyForm(BaseForm):
-    
-    class Meta:
-       model = CompanyModel
-       fields = '__all__'
-
-
-class CategoryCompanyForm(BaseForm):
-    
-    class Meta:
-       model = CategoryCompanyModel
-       fields = '__all__'
-
-    
-class ContactCompanyForm(BaseForm):
-    
-    class Meta:
-       model = ContactCompanyModel
-       fields = '__all__'
-
-
-class GaleryCompanyForm(BaseForm):
-    
-    class Meta:
-       model = GaleryCompanyModel
-       fields = '__all__'
-"""       
-
-
 
--- company/views/render_views.py
+++ company/views/render_views.py
@@ -1,20 +1,15 @@
 from django.views.generic.base import TemplateView
 from company.forms import RegisterCompanyForm
-# from company.forms import CompanyForm, GaleryCompanyForm, CategoryCompanyForm, ContactCompanyForm
 
 from user.forms import ProfileUserForm
 
 class CompanyTemplateView(TemplateView):
     template_name = "company.html"
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_context_data(self):
+        context = super().get_context_data()
         context['form_register_company'] = RegisterCompanyForm()
-        # context['form_company'] = CompanyForm()
-        # context['form_galery_company'] = GaleryCompanyForm()
-        # context['form_contact_company'] = ContactCompanyForm()
-        # context['form_category_company'] = CategoryCompanyForm()
-        # context['form_profile_user'] = ProfileUserForm()
+        
         return context
 
 
